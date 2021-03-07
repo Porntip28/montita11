4ddb58cbc984190b0461752f50367c00a1412e48
Porntip28 <68530012+Porntip28@users.noreply.github.com>
2021-03-07 17:54:39 +0700

    Add files via upload

---
+++ mon/admin/manage/index.php
@@ -0,0 +1,25 @@
+<h7><center>จัดการข้อมูลการรักษา</center></h7>
+<p>
+<div class="card-group">
+<div class="card">
+  <a href="?file=manage/treat/index">
+    <img class="card-img-top" src="image/help.png" alt="Card image cap"></a> 
+    <div class="card-body">
+    <h5 class="card-title"><center>จัดการข้อมูลการรักษา</center></h5>  </div>
+  </div>
+  <div class="card">
+    <!-- <a href="?file=pill/pill-part/index"> -->
+    <!-- <img class="card-img-top" src="image/pill.png" alt="Card image cap"></a> -->
+    <div class="card-body">
+      <h5 class="card-title"><center></center></h5>  </div>
+  </div>
+  <div class="card">
+    <!-- <a href="?file=pill/order_pill/history_order_pill">
+    <img class="card-img-top" src="image/order.png" alt="Card image cap"></a> -->
+    <div class="card-body">
+      <h5 class="card-title"><center></center></h5>  </div>
+  </div>
+  
+</div>
+</div>
+<p><p>
